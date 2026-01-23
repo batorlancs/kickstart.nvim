79a00e91dea1fdbd38e0d933e95474ec5f4206b3
batorlancs <batorgergely3@gmail.com>
2026-01-23 21:12:16 +0100

    feat: diagnostics keymaps and snacks update

--- lua/custom/plugins/diagnostics.lua
+++ lua/custom/plugins/diagnostics.lua
@@ -3,13 +3,10 @@ return {
   event = 'VeryLazy',
   priority = 1000,
   config = function()
-    require('tiny-inline-diagnostic').setup {
-      -- options = {
-      --   multilines = {
-      --     enabled = true,
-      --   },
-      -- },
-    }
+    require('tiny-inline-diagnostic').setup {}
     vim.diagnostic.config { virtual_text = false } -- Disable Neovim's default virtual text diagnostics
+    vim.keymap.set('n', '<leader>de', '<cmd>TinyInlineDiag enable<cr>', { desc = 'Enable diagnostics' })
+    vim.keymap.set('n', '<leader>dd', '<cmd>TinyInlineDiag disable<cr>', { desc = 'Disable diagnostics' })
+    vim.keymap.set('n', '<leader>dt', '<cmd>TinyInlineDiag toggle<cr>', { desc = 'Toggle diagnostics' })
   end,
 }
--- lua/custom/plugins/snacks.lua
+++ lua/custom/plugins/snacks.lua
@@ -1,10 +1,38 @@
 return {
   'folke/snacks.nvim',
-  ---@type snacks.Config
+  priority = 1000,
+  lazy = false,
+  init = function()
+    vim.api.nvim_create_autocmd('VimEnter', {
+      callback = function()
+        if vim.fn.argc() == 0 and vim.fn.line2byte(vim.fn.line '$') == -1 then
+          Snacks.dashboard()
+        end
+      end,
+    })
+  end,
   opts = {
-    lazygit = {},
+    lazygit = {
+      enabled = true,
+    },
+    dashboard = {
+      enabled = true,
+      sections = {
+        { section = 'header' },
+        { icon = ' ', title = 'Keymaps', section = 'keys', indent = 2, padding = 1 },
+        { icon = ' ', title = 'Recent Files', section = 'recent_files', indent = 2, padding = 1 },
+        { icon = ' ', title = 'Projects', section = 'projects', indent = 2, padding = 1 },
+        { section = 'startup' },
+      },
+    },
   },
   keys = {
-    { '<leader>gg', function() Snacks.lazygit() end, desc = 'Lazygit' },
+    {
+      '<leader>gg',
+      function()
+        Snacks.lazygit()
+      end,
+      desc = 'Lazygit',
+    },
   },
 }

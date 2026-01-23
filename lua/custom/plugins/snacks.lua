4b561c26e536cb819b4b31aa2215b8be1b63b7de
batorlancs <batorgergely3@gmail.com>
2026-01-23 22:59:01 +0100

    feat: update snacks dashboard

--- lua/custom/plugins/snacks.lua
+++ lua/custom/plugins/snacks.lua
@@ -6,14 +6,52 @@ return {
     lazygit = {
       enabled = true,
     },
+    gh = {},
+    picker = {
+      sources = {
+        gh_issue = {},
+        gh_pr = {},
+      },
+    },
     dashboard = {
       enabled = true,
+      preset = {
+        header = [[NVIM]],
+      },
       sections = {
         { section = 'header' },
-        { icon = ' ', title = 'Keymaps', section = 'keys', indent = 2, padding = 1 },
         { icon = ' ', title = 'Recent Files', section = 'recent_files', indent = 2, padding = 1 },
         { icon = ' ', title = 'Projects', section = 'projects', indent = 2, padding = 1 },
-        { section = 'startup' },
+        {
+          icon = ' ',
+          desc = 'Browse Repo',
+          padding = 1,
+          key = 'b',
+          action = function()
+            Snacks.gitbrowse()
+          end,
+        },
+        function()
+          local in_git = Snacks.git.get_root() ~= nil
+          local cmds = {
+            {
+              icon = ' ',
+              title = 'Git Status',
+              cmd = 'git --no-pager diff --stat -B -M -C',
+              height = 10,
+            },
+          }
+          return vim.tbl_map(function(cmd)
+            return vim.tbl_extend('force', {
+              section = 'terminal',
+              enabled = in_git,
+              padding = 1,
+              ttl = 5 * 60,
+              indent = 3,
+            }, cmd)
+          end, cmds)
+        end,
+        -- { section = 'startup' },
       },
     },
   },
@@ -24,6 +62,34 @@ return {
         Snacks.lazygit()
       end,
       desc = 'Lazygit',
+    },
+    {
+      '<leader>gi',
+      function()
+        Snacks.picker.gh_issue()
+      end,
+      desc = 'GitHub Issues (open)',
+    },
+    {
+      '<leader>gI',
+      function()
+        Snacks.picker.gh_issue { state = 'all' }
+      end,
+      desc = 'GitHub Issues (all)',
+    },
+    {
+      '<leader>gp',
+      function()
+        Snacks.picker.gh_pr()
+      end,
+      desc = 'GitHub Pull Requests (open)',
+    },
+    {
+      '<leader>gP',
+      function()
+        Snacks.picker.gh_pr { state = 'all' }
+      end,
+      desc = 'GitHub Pull Requests (all)',
     },
   },
 }
